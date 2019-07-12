88dbe9529da1fb461c792a14cddca28ba2af28b1
Evita Lobo <elobo@ucsc.edu>
2019-07-12 13:43:32 -0700

    working world

--- p5/asg2/sketch.js
+++ p5/asg2/sketch.js
@@ -4,23 +4,55 @@
 
   //let worldSize =  new Array(999999999);
   let world;
+  let side=0;
+  let up=0;
+  let cam;
+
+
 
 function setup() {
   createCanvas(500,500,WEBGL);
-  frameRate(30);
+  frameRate(60);
   pixelDensity(1);
   fill(0);
+  //noiseSeed(99);
+   cam = createCamera();
+   
 
-  world = new WorldGenerator(0, 32, 32, 32); 
+  // set initial tilt angle
+  cam.tilt(0);
+  cam.pan(0);
+ //cam.tilt(mouseY);
+  world = new WorldGenerator(this.seed, 32, 32, 32); 
 
 }
 
 function draw() {
   clear();
+  let tiltAng= map(mouseX,0,500,0,2*PI)
+  cam.tilt(tiltAng);
+   let panAng= map(0,mouseY,500,0,2*PI)
+  cam.pan(panAng);
   world.run();
+  // cam.setPosition(x, y,z);
+console.log(cam);
 
- // camera(0, 40, 40, 0, 0, 0, 0, 4, 0);
+//camera(0, 0, 0, 0, 0, 0, 0, 4, 0);
+
+ //  7 camera(mouseX, mouseY, side, 0, 0, 0, 0, 4, 0);
  // worldSize[i];.display();
 
 }
 
+function keyPressed() {
+  let keyIndex = -1;
+  if (key >= 'a') {
+    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
+   
+  }
+}
+
+
+  // key <= 'z') {
+
+//let camera = vector 
--- p5/asg2/world-generator.js
+++ p5/asg2/world-generator.js
@@ -11,9 +11,10 @@ If the same seed is given is input, the same world should be generated.  */
 class WorldGenerator{
 	constructor(seed, width, height, depth) {
 		this.brickSize = 10;
+		//bricksize=6 or 16
 
-		this.seed = seed;//make it so that you can take a string and create a new world
-   		seed = str(seed);
+		noiseSeed(9);//make it so that you can take a string and create a new world
+   		//seed = 0;
    		//textures
 		this.deep = loadImage('textures/deep.png');
 		this.shallow = loadImage('textures/shallow.png');
@@ -23,26 +24,30 @@ class WorldGenerator{
 		this.mountain = loadImage('textures/mountain.png');
 		this.ice = loadImage('textures/ice.png');
 
-   		//save(seed);
+   		
         this.width = width;
         this.height = height;
         this.depth = depth;
         this.world3d = [];
-        let threshold = .3;
+        let threshold = .7;
         //adjust threshold for intensity
       
         // store world height,width and depth in world3d array
         for(let y = 0; y <height; y++){ 
         	this.world3d[y] = [];
     		for( let x = 0; x <width; x++){
+
+
+    			let noiseFunc = noise(x/10 ,y/10) *depth;
+				//console.log(noiseFunc)
     			this.world3d[y][x] = [];
 				for(let z = 0; z <depth; z++){ 
 					this.world3d[y][x][z] = false;
 					//use noise to define height
-					let noiseFunc = noise(x/10 ,y/10, z/10);
+					//noise(x,yy) + (32 *6)
+					
 					//height = noiseFunc;
-					if(noiseFunc < threshold) {
-						
+					if(noiseFunc > z) {
 						this.world3d[y][x][z] = true; //box(x,y,z);
 						//world3d[y][x][z].setTexture(whatever);
 					}
@@ -61,45 +66,38 @@ class WorldGenerator{
 	     	for(let y=0;y<this.height;y++) {
 	     		for(let x=0;x<this.width;x++) {
 
-					for(let z=0;z<this.depth;z++) {
+					for(let z=0;z<this.depth/2;z++) {
+						/*if(random > 0.01){
+							continue;
+						}*/
+						//console.log(z);
+						if(!this.world3d[y][x][z]){
+							continue;
+						}
+						
 						//set z depth value to see how far away from top of world
 			 			if(z >= 0 && z < 3) {
-			 				if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
-	     					texture(this.deep);
-	     				}
+			 			texture(this.deep);
 					  }else if (z >= 3 && z < 6){
-							if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
+	     					//box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
 	     					texture(this.shallow);
-	     				}
 				      }else if (z >= 6 && z < 12){
-				       	if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
 	     					texture(this.sand);
-	     				}
 				      }else if (z >= 12 && z < 17){
-				       	if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
+				       //box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
 				        	texture(this.grass);
-				        }
 				      }else if (z >= 17 && z < 23){
-				       	if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
 				       		texture(this.stone);
-				       	}
 				      }else if (z >= 23 && z < 28){
-				       	if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
 				       		texture(this.mountain);
-				       	}
 				      } else {
-				       	if(this.world3d[y][x][z]) {
-	     					box(y*this.brickSize, x*this.brickSize, z*this.brickSize);
+				      		//console.log(z);
 				       		texture(this.ice);
-				       	}
 				      }
-		     			
+				      	push();
+				      	translate(y*this.brickSize, x*this.brickSize, z*this.brickSize);
+		     			box(this.brickSize,this.brickSize,this.brickSize);
+		     			pop();
      			}
      		}
      	}
