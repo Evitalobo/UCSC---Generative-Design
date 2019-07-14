41bad8755fdf80e0c6a967ef5c2a32b4ec49eb21
Evita Lobo <elobo@ucsc.edu>
2019-07-13 21:17:17 -0700

    Finalized verion

--- p5/asg2/sketch.js
+++ p5/asg2/sketch.js
@@ -4,9 +4,9 @@
 
   //let worldSize =  new Array(999999999);
   let world;
-  let sideX=-20;
-  let sideY= -20;
-  let sideZ= 200;
+  let sideX=-50;
+  let sideY= -50;
+  let sideZ= 400;
   let eyeX= 100;
   let eyeY =100;
   let eyeZ = 100;
@@ -17,105 +17,77 @@ let forward= -400;
 function setup() {
   createCanvas(500,500,WEBGL);
   frameRate(60);
-  //pixelDensity(1);
+  pixelDensity(1);
   fill(0);
 
-  //noiseSeed(99);
+//create a camera object
   cam = createCamera();
   cam.move(-300,-300,-130);
   cam.camera(sideX,sideY,sideZ,eyeX,eyeY,eyeZ,-300,-300,0);
-  //cam.lookAt(200,200,-200);
-/*
-  let tiltAng= map(mouseX,0,500,0,2*PI)
-  cam.tilt(tiltAng);
-
-  // set initial tilt angle
-  let panAng= map(0,mouseY,500,0,2*PI)
-    cam.pan(panAng);
-  */
- //cam.tilt(mouseY);
+ 
+ //generate new world
   world = new WorldGenerator(this.seed, 32, 32, 32); 
-
-  //cam(;
 
 }
 
 
 function draw() {
+  //draw the world each time after it is generated to update camera
   clear();
-
-  //camera(side,-200,forward,0,0,0,0,0,0);
   world.run();
 
-  //  let tiltAng= map(mouseX,0,500,0,2*PI)
-  //cam.tilt(tiltAng);
-  //cam.tilt()
+  //camera updates based on interaction
   cam.camera(sideX,sideY,sideZ,eyeX,eyeY,eyeZ,-300,-300,0);
+}
 
-  let dx = -(pmouseX - mouseX)/1.0;
-  let dy = -(pmouseY - mouseY)/1.0;
-  cam.pan(dx);
-  cam.tilt(dy);
-
-
-/*
-  //Mouse rotation
-  if(mouseX >= 250){
-    cam.tilt(.01);
-  }else if (mouseX < 250){
-    cam.tilt(-0.01);
-  }
-
-  if(mouseY >= 250){
-    cam.pan(.01);
-  }else if (mouseY < 250){
-    cam.pan(-0.01);
-  }
-  
- //  cam.pan(-.01); 
-   
-  // cam.setPosition(x, y,z);
-//console.log(cam);
-*/
-
+//Drag mouse to rotate camera view
+function mouseDragged(event){
+   let dx = -( mouseX -pmouseX)/500 ;
+   let dy = -(pmouseY - mouseY)/500;
+    cam.pan(dx);
+    cam.tilt(dy);
+  //console.log( "Dx:" +dx)
+  //console.log( "Dy:" +dy)
 
 }
 
+//WASD keys allow for FP movement
 function keyPressed() {
-  if (key >= 'a' && key <= 'z') {
-    keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
-  }
+    if (key >= 'a' && key <= 'z') {
+      keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
+    }
+
   //D key moves to right side
    if (key =='d'){
-   sideY= sideY +5;
-   sideX= sideX -5;
-   eyeY = eyeY +5;
-    eyeX= eyeX -5;
-   console.log("d key:");
+       sideY= sideY +5;
+       sideX= sideX -5;
+       eyeY = eyeY +5;
+       eyeX= eyeX -5;
+   //console.log("d key:");
     }
     //A key moves to left side
      if (key =='a'){
-  sideY= sideY -5;
-   eyeY = eyeY -5;
-   sideX= sideX +5;
-   eyeX= eyeX +5;
-   console.log("a key:");
+         sideY= sideY -5;
+         eyeY = eyeY -5;
+         sideX= sideX +5;
+         eyeX= eyeX +5;
+   //console.log("a key:");
     }
     //W key moves forward
      if (key =='w'){
-   sideX= sideX +5;
-      sideY= sideY +5;
-   eyeX = eyeX +5;
-   eyeY = eyeY +5; 
-   console.log("w key:");
+       sideX= sideX +5;
+       sideY= sideY +5;
+       eyeX = eyeX +5;
+       eyeY = eyeY +5; 
+  // console.log("w key:");
     }
     //S key moves backward
     if (key =='s'){
-   sideX= sideX -5;
-      sideY= sideY -5;
-   eyeX = eyeX -5;
-   eyeY = eyeY -5;
-   console.log("s key:");
+       sideX= sideX -5;
+       sideY= sideY -5;
+       eyeX = eyeX -5;
+       eyeY = eyeY -5;
+   //console.log("s key:");
     }
   }
 
--- p5/asg2/world-generator.js
+++ p5/asg2/world-generator.js
@@ -10,11 +10,14 @@ If the same seed is given is input, the same world should be generated.  */
 
 class WorldGenerator{
 	constructor(seed, width, height, depth) {
-		this.brickSize = 10;
-		//bricksize=6 or 16
+		//brick width,height and depth
+		this.brickSize = 16;
+	
+		//edit number to change seed val
+		noiseSeed(90);
+   		
 
-		noiseSeed(90);//make it so that you can take a string and create a new world
-   		//seed = 0;
+
    		//textures
 		this.deep = loadImage('textures/deep.png');
 		this.shallow = loadImage('textures/shallow.png');
@@ -29,27 +32,25 @@ class WorldGenerator{
         this.height = height;
         this.depth = depth;
         this.world3d = [];
-        //let threshold = .7;
-        //adjust threshold for intensity
+       
       
         // store world height,width and depth in world3d array
         for(let y = 0; y <height; y++){ 
         	this.world3d[y] = [];
     		for( let x = 0; x <width; x++){
 
-
+				//use noise to define depth from top (noise takes in height and width parameters)
     			let noiseFunc = noise(x/10,y/10) *depth ;
 				//console.log(noiseFunc)
     			this.world3d[y][x] = [];
 				for(let z = 0; z <depth; z++){ 
 					this.world3d[y][x][z] = false;
-					//use noise to define height
+					
 					//noise(x,yy) + (32 *6)
 					
-					//height = noiseFunc;
+					//Checks to see if the noise value is more than intial depth(0), if so then stores potential values
 					if(noiseFunc > z) {
-						this.world3d[y][x][z] = true; //box(x,y,z);
-						//world3d[y][x][z].setTexture(whatever);
+						this.world3d[y][x][z] = true; 
 					}
 				
 		        }
@@ -59,26 +60,22 @@ class WorldGenerator{
 	run() {
 	    //this.update();
 	    this.display();
-	    //noLoop();
 	}
 
     // Method to display
     display() {
 
-    		strokeWeight(1);
+    		strokeWeight(.8);
 	     	for(let y=0;y<this.height;y++) {
 	     		for(let x=0;x<this.width;x++) {
-
+					//set z depth value to see how far away from top of world
 					for(let z=0;z<this.depth;z++) {
-						/*if(random > 0.01){
-							continue;
-						}*/
-						//console.log(z);
+						//if there is nothing, render no cubes
 						if(!this.world3d[y][x][z]){
 							continue;
 						}
 						
-						//set z depth value to see how far away from top of world
+						//based on the z position for each cube in the this.world3d[y][x] array, chose a texture
 			 			if(z >= 0 && z < 8) {
 			 			texture(this.deep);
 					  }else if (z >= 8 && z < 12){
@@ -94,9 +91,9 @@ class WorldGenerator{
 				      }else if (z >= 21 && z < 23){
 				       		texture(this.stone);
 				      } else {
-				      		//console.log(z);
 				       		texture(this.ice);
 				      }
+				      //push and pop the stack of cubes and move it based on the position based on the world array
 				      	push();
 				      	translate(y*this.brickSize, x*this.brickSize, z*this.brickSize);
 		     			box(this.brickSize,this.brickSize,this.brickSize);
