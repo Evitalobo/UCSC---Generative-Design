a3f925aa83453245f076cf516d3d9844602eafc0
Evita Lobo <elobo@ucsc.edu>
2019-07-22 14:30:08 -0700

    Started working on Asg4

---
+++ p5/asg4/ga.js
@@ -0,0 +1,143 @@
+//is the fitnessfunc supposed to be the entire race?? it is evaluated twice in the evolve()func
+
+class Individual {
+    constructor(indSize) {
+        this.indSize = indSize;
+        this.gens = new Array(indSize);
+        this.fitness = 0;
+
+        this.init();
+    }
+
+    //the init() population initialization method. Currently, the init() function generates genotypes as random binary arrays. 
+    //The Car class has a function randomFeatures() that helps you initializing cars.
+    init() {
+        for(let i = 0; i < this.indSize; i++) {
+            this.gens[i] = int(random(2));
+
+        }
+    }
+}
+
+class GeneticAlgorithm {
+    constructor(popSize, indSize, fitFunc, mutationRate, mutationFunc) {
+        this.indSize = indSize;
+        this.popSize = popSize;
+        this.fitFunc = fitFunc;
+        this.mutationRate = mutationRate;
+        this.mutationFunc = mutationFunc;
+        this.init();
+    }
+
+    init() {
+        this.population = new Array(this.popSize);
+        for(let i = 0; i < this.popSize; i++) {
+            // Initialize individual i randomly
+            this.population[i] = new Individual(this.indSize);
+        }
+    }
+
+    //reproduction occurs and chose best candiate
+    evolve() {
+        this.evaluate();
+
+        let matingPool = this.select();
+        let newPopulation = this.reproduce(matingPool);
+        this.mutate(newPopulation);
+
+        this.population = newPopulation;
+        //double evaluation
+       // this.evaluate();
+        return this.best();
+    }
+
+    evaluate() {
+        for(let i = 0; i < this.popSize; i++) {
+            let individual = this.population[i];
+            individual.fitness = this.fitFunc(individual.gens);
+        }
+    }
+
+    select() {
+        let matingPool = new Array();
+
+        // Select this.popSize Individual to be the parents
+        for(let i = 0; i < this.popSize; i++) {
+            let survivor = this.rouletteWheel();
+            matingPool.push(survivor);
+        }
+
+        return matingPool;
+    }
+
+    //the rouletteWheel() function to perform probabilistic selection.
+    rouletteWheel() {
+        //add all fitness scores
+        let sum = this.population.map(function(ind){
+                  return ind.fitness;
+                 }).reduce(function(a,b){
+                    return (a + b);
+                 });
+        //find percentages of each individual based on the sum- an array of probabilities
+        let prob = this.population.map(function(ind){
+            return ind.fitness/sum;
+        });
+        //Randomize and pick whichever one is selected
+        let rand = Math.random();
+        for(let i =0; i < prob.length; i++){
+            if(rand < prob[i]){
+                return this.population[i];
+            }
+            rand -= prob[i];
+        }
+    }
+
+    reproduce(matingPool) {
+        let newPopulation = new Array(this.popSize);
+
+        for(let i = 0; i < this.popSize; i++) {
+            let a = int(random(this.popSize));
+            let b = int(random(this.popSize));
+
+            newPopulation[i] = this.crossover(matingPool[a], matingPool[b]);
+        }
+
+        return newPopulation;
+    }
+
+    //the crossover() function  to perform your defined crossover operation (e.g. one-point crossover) on a pair of parents.
+    crossover(parentA, parentB) {
+        let ranA = Math.floor(Math.random() * parentA.gens.length);
+        let ranB = Math.floor(Math.random() * parentB.gens.length);
+        //ranA will always be <= ran B
+        [ranA, ranB] = [Math.min(ranA,ranB),Math.max(ranA,ranB)];
+
+        let genA = parentA.gens.slice(ranA,ranB);
+        let genB = parentB.gens.slice(ranA,ranB);
+
+        //crossover to pick either floor of ranA or ranB
+        parentA.gens = parentA.gens.slice(0,ranA).concat(genB).concat(parentA.gens.slice(ranB));
+        parentB.gens = parentB.gens.slice(0,ranA).concat(genA).concat(parentB.gens.slice(ranB));
+        //50% chance of getting either gene
+        return Math.random() < 0.5 ? parentA : parentB;
+    }
+
+    //the mutate() function to mutate your population of cars.
+    mutate(newPopulation) {
+        for(let ind of newPopulation){
+             let newgens = this.mutationFunc();
+             for(let i in ind.gens){
+                if(Math.random() < this.mutationRate){
+                    ind.gens[i] = newgens[i];
+                }
+             }
+        }
+    }
+
+    //  the best() function to return the best car after one generation().
+    best() {
+       return this.population.sort(function(a, b){    
+            return b.fitness - a.fitness ;
+        })[0].gens;
+    }
+}
---
+++ p5/asg4/sketch.js
@@ -0,0 +1,106 @@
+let world;
+let race;
+let camera;
+let GA;
+
+function setup() {
+  createCanvas(640, 400, WEBGL);
+  setAttributes('antialias', true);
+
+  // Initialize box2d physics and create the world
+  world = createWorld();
+  camera = createCamera();
+
+  // Create Camera
+  camera.ortho(-width / 2, width / 2, -height / 2, height /2, 0, 10);
+  camera.setPosition(0, 0, 0);
+
+  // Create a list of cars
+  let cars = []
+  for(let i = 0; i < 10; i++) {
+      let feats = Car.randomFeatures();
+      let pos = createVector(0, -100);
+      let car = new Car(pos.x, pos.y, "car" + i, feats);
+      cars.push(car);
+  }
+
+  // Create a terrain
+  let pos = createVector(-width/2, 10);
+  terrain = new Terrain(pos.x, pos.y, 100, 100, 1);
+
+  // Create a world to manage the cars
+  race = new Race(terrain, cars, raceOverCallback);
+  race.start();
+}
+
+function draw() {
+    if (race.running) {
+        background(240);
+    }
+
+    race.update();
+    race.draw();
+
+    if(race.running) {
+        // Update physics. 2nd and 3rd arguments are velocity and position iterations
+        let timeStep = 1.0 / 30;
+        world.Step(timeStep, 10, 10);
+
+        // Get race leaderboards
+        let leaderboard = race.getLeaderboards();
+
+        // Follow first car with the camera
+        let firstCar = leaderboard[0].car;
+
+        if (firstCar) {
+            let firstPos = firstCar.getPosition();
+            camera.setPosition(firstPos.x + width/5, firstPos.y, camera.eyeZ);
+        }
+    }
+}
+
+// ========================================
+// Callback function for when the race is over
+// ========================================
+function raceOverCallback(finalLeaderboards) {
+    console.log("race over!");
+    console.log(finalLeaderboards);
+
+    //GA instantiation of Genetic Algorithms class
+    let popSize = 10;
+    let indSize = 20;
+    let fitFunc = function(gens){
+     let car = finalLeaderboards.filter(function(c){
+        return JSON.stringify(c.car.feats) === JSON.stringify(gens);
+     });
+     return car.progress;
+    }
+
+    let mutationRate = 0.01;
+    let mutationFunc = Car.randomFeatures;
+
+    GA = new GeneticAlgorithm(popSize, indSize, fitFunc, mutationRate, mutationFunc);
+
+    for (let i=0 ; i < GA.population.length ; i++){
+      let ind = GA.population[i];
+      ind.gens = finalLeaderboards[i].car.feats;
+    }
+    //evolve and best is not going to work since it happens at ga.js line 49
+    let best = GA.evolve();
+
+    // Restart race with new cars
+    let cars = []
+    for(let i = 0; i < 10; i++) {
+        let feats = Car.GeneticAlgorithm();
+        let pos = createVector(0, -100);
+        let car = new Car(pos.x, pos.y, "car" + i, feats);
+        cars.push(car);
+    }
+
+    race.setCars(cars);
+    race.start();
+
+
+
+
+}
