288bab9da9e88f04de724d27de0b4ba5996e7246
Evita Lobo <elobo@ucsc.edu>
2019-08-01 12:48:21 -0700

    Finished Markov class

--- p5/asg5/sketch.js
+++ p5/asg5/sketch.js
@@ -32,15 +32,25 @@ function onButtonClicked() {
 }
 
 function onMIDIsLoaded(pianoRolls) {
-    // Pick random file to play
-    let pianoRoll = random(pianoRolls);
-
+    //get data for Markhov model
+    let modelData = midiPlayer.pianoRolls.map(midiPlayer.pianoRoll2Text);
+    //create Markhov model and feed it modelData
+    let markhov = new Markhov(modelData);
+    //use Markhov to create string
+    //let rollText = "62_4 . . . . . 61_4 64_4 . . . . . 60_4 66_4 . . . . . 59_4 67_4";
+    let rollText = markhov.create();
+    //convert string to midi
+    let midiData = midiPlayer.text2Midi(rollText);console.log(midiData);
+    //convert midi to pianoRoll
+    let pianoRoll = midiPlayer.notes2PianoRoll(midiData.duration, midiData.tracks[0].notes);
+    //play pianoRoll
     midiPlayer.setPianoRoll(pianoRoll, tsCallback);
 
     // Encode the piano roll (2D array) as string
     let midiText = midiPlayer.pianoRoll2Text(pianoRoll);
-    console.log(midiText);
+    //console.log(midiText);
 }
+
 
 function tsCallback(currentTs, notesOn) {
     // console.log(currentTs, notesOn);
