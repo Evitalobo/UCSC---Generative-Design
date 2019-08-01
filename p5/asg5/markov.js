c8bd6114071d644f3116322922fa14239c4957d5
Evita Lobo <elobo@ucsc.edu>
2019-08-01 11:07:19 -0700

    Base code

---
+++ p5/asg5/markov.js
@@ -0,0 +1,3 @@
+//markov.js
+//Implement (in "markov.js") a Markov Chain class which supports training a markov chain on a given 
+//dataset and sampling from it to generate new pieces on the style of the dataset.
